bec5cf2efd7f40f8b34ad2793029a6b1ca2cb383
StevenChen17 <00091523@uca.edu.sv>
2025-10-23 17:18:13 -0600

    Agregar codigo

--- Parcial_2_PW/app.js
+++ Parcial_2_PW/app.js
@@ -11,8 +11,42 @@ app.listen(PORT, () => {
 });
 
 app.get("/cuentas", (req, res) => {
+  const { id, nombre, genero  } = req.query;
+
+  let resultado = cuentas;
+
+  if(id) {
+    resultado = resultado.filter(c => c._id === id)
+  }
+
+  if(nombre) {
+    const partes = nombre.toLowerCase().split(" ");
+    resultado = resultado.filter(c => 
+    partes.every(p => c.client.toLowerCase().includes(p))
+);
+
+  }
+
+  if(genero) {
+    resultado = resultado.filter(c => c.gender === genero)
+  }
+
   res.send({
-    count: cuentas.length,
-    data: cuentas
+    count: resultado.length,
+    cuentas: resultado
   });
 });
+
+
+const cuentaExiste = (cuenta) => cuenta !== null && cuenta !== undefined;
+
+app.get("/cuentas/:id", (req, res) => {
+  const cuentaId = req.params.id;
+  const cuenta = cuentas.find(c => c._id === cuentaId);
+
+  res.send({ 
+    find: cuentaExiste(cuenta),
+    account: cuenta
+  });
+});
+
