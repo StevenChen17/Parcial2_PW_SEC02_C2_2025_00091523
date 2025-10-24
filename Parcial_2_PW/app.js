303672268fe0465873537ca87d8d60994411742f
StevenChen17 <00091523@uca.edu.sv>
2025-10-23 18:40:21 -0600

    Agregar mas codigo

--- Parcial_2_PW/app.js
+++ Parcial_2_PW/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cuentas = require('./cuentas.json'); 
+const cuentas = require('./cuentas.json');
 
 const app = express();
 app.use(express.json());
@@ -10,43 +10,80 @@ app.listen(PORT, () => {
   console.log("Server Listening on PORT:", PORT);
 });
 
+const cuentaExiste = (cuenta) => cuenta !== null && cuenta !== undefined;
+
+function accountBalance(cuentas, total = 0) {
+  cuentas.forEach(cuenta => {
+    if (cuenta.isActive) {
+      const balanceNum = parseFloat(cuenta.balance.replace(/[^0-9.-]+/g, ""));
+      total += balanceNum;
+    }
+  });
+  return total;
+}
+
+
 app.get("/cuentas", (req, res) => {
-  const { id, nombre, genero  } = req.query;
+  const { id, nombre, genero } = req.query;
 
   let resultado = cuentas;
 
-  if(id) {
+  if (id) {
     resultado = resultado.filter(c => c._id === id)
   }
 
-  if(nombre) {
+  if (nombre) {
     const partes = nombre.toLowerCase().split(" ");
-    resultado = resultado.filter(c => 
-    partes.every(p => c.client.toLowerCase().includes(p))
-);
-
+    resultado = resultado.filter(c =>
+      partes.every(p => c.client.toLowerCase().includes(p))
+    );
   }
 
-  if(genero) {
+  if (genero) {
     resultado = resultado.filter(c => c.gender === genero)
   }
 
+  const size = resultado.length;
+  const response = {}
+
+  // Solo incluir count si NO hay filtros activos
+  const sinFiltros = Object.keys(req.query).length === 0;
+  if (sinFiltros) {
+    response.count = cuentas.length;
+  }
+
+  if (size === 1) {
+    response.finded = true;
+    response.account = resultado[0];
+  } else if (size > 1) {
+    response.data = resultado;
+  } else {
+    response.finded = false;
+  }
+
   res.send({
-    count: resultado.length,
-    cuentas: resultado
+    response
   });
 });
 
 
-const cuentaExiste = (cuenta) => cuenta !== null && cuenta !== undefined;
-
-app.get("/cuentas/:id", (req, res) => {
+app.get("/cuenta/:id", (req, res) => {
   const cuentaId = req.params.id;
   const cuenta = cuentas.find(c => c._id === cuentaId);
 
-  res.send({ 
+  res.send({
     find: cuentaExiste(cuenta),
     account: cuenta
   });
 });
 
+
+app.get("/cuentasBalance", (req, res) => {
+  const cuenta = cuentas.find(c => c.isActive === true);
+  const total = accountBalance(cuentas);
+
+  res.send({
+    status: cuentaExiste(cuenta),
+    accountBalance: total
+  });
+});
